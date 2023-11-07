52dd61084ec3e351258d5207f25cea24b2998fa2
William Addesso <wsaddesso@gmail.com>
2023-11-06 22:34:54 -0500

    Added content to index.html and about.html. Added check for url.

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 const http = require('http');
+const path = require('path');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
   // Handle request and generate response here.
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello, World!\n');
+  if (req.url === '/') {
+    fs.readFile(
+      path.join(__dirname, 'public', 'index.html'),
+      (err, content) => {
+        if (err) throw err;
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(content);
+        res.end();
+      }
+    );
+  }
+
+  if (req.url === '/about') {
+    fs.readFile(
+      path.join(__dirname, 'public', 'about.html'),
+      (err, content) => {
+        if (err) throw err;
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(content);
+        res.end();
+      }
+    );
+  }
 });
 
 const port = 3000;
